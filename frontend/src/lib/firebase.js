c16701cfea78dfef6a028ddb43cb94d0ff11032b
Sebastian Utomo <seapanda47@gmail.com>
2026-06-02 21:30:51 +0700

    Added firebase

---
+++ frontend/src/lib/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCZqgGaOxYFmGrwUBlYhK2_BAIC2vyLZpY",
+  authDomain: "popit-38f02.firebaseapp.com",
+  projectId: "popit-38f02",
+  storageBucket: "popit-38f02.firebasestorage.app",
+  messagingSenderId: "1085684768577",
+  appId: "1:1085684768577:web:30fe0264209d2f41384bc0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
