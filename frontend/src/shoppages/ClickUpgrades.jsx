0496d7eae8dd26611cc78e9623b5cb4cec4458e5
vruaaan <brianp.2680@gmail.com>
2026-06-09 19:29:32 +0800

    updated pngs

--- frontend/src/shoppages/ClickUpgrades.jsx
+++ frontend/src/shoppages/ClickUpgrades.jsx
@@ -1,7 +1,7 @@
 import threechips from '../assets/threechips.png'
 const CLICK_UPGRADES = [
-  { id: 1, name: "Lazy Feeder", desc: "Free your hands, automatically popping chips", baseCost: 50,  costScale: 1.5, powerPerLevel: 1 },
-  { id: 2, name: "Flavour Town",  desc: "Flavour your chips with each upgrade, increasing the value of each chip popped !", baseCost: 150, costScale: 1.8, powerPerLevel: 2 },
+  { id: 1, name: "Click...Click...Boom!", desc: "Free your hands, automatically popping chips", baseCost: 50,  costScale: 1.5, powerPerLevel: 1 },
+  { id: 2, name: "Salt 'n Pepper Shaker",  desc: "Flavour your chips with each upgrade, increasing the value of each chip popped !", baseCost: 150, costScale: 1.8, powerPerLevel: 2 },
 ];
 
 function calcCost(baseCost, costScale, level) {
